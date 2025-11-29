f549b8f3553bb83a5f324bb385eeeb848ad99172
actualview25 <actual.view25@gmail.com>
2025-11-29 04:39:14 +0300

    Update data.js

--- data.js
+++ data.js
@@ -16,46 +16,23 @@ window.APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
         }
       ],
       "faceSize": 2000,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "fov": 1.57
       },
       "linkHotspots": [],
-      "infoHotspots": [
-        {
-          "yaw": 0.384894776755317,
-          "pitch": -0.13116248945829945,
-          "title": "جليك",
-          "text": "rr"
-        },
-        {
-          "yaw": 0.7917131110213589,
-          "pitch": 0.1945176818278398,
-          "title": "Title",
-          "text": "Text"
-        },
-        {
-          "yaw": -1.7105826779075457,
-          "pitch": -0.007674382656263745,
-          "title": "Title",
-          "text": "Text"
-        }
-      ]
+      "infoHotspots": []
     }
   ],
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
-    "fullscreenButton": true,
-    "viewControlButtons": true
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
   }
 };
