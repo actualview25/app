ba368953dd80ae42298759763ffdb9389d98f718
actualview25 <actual.view25@gmail.com>
2025-11-29 05:46:11 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,16 +1,24 @@
 window.APP_DATA = {
   "scenes": [
     {
-      "id": "0-prifit_reception", 
+      "id": "0-prifit_reception",
       "name": "PRiFit Reception",
       "levels": [
         {
           "tileSize": 256,
           "size": 256,
           "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
         }
       ],
-      "faceSize": 1000,
+      "faceSize": 2000,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
